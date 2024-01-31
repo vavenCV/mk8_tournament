57b78281bead703123d9b2a40f0dbe501f6242c9
Matthias Dubouchet <beulard@gmail.com>
2024-01-31 01:31:23 +0100

    components, assets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,26 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#F7186B",
+        },
+        secondary: {
+            main: "#3ADCF5",
+        },
+    },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+);
